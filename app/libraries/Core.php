9022f500279496751500291a72466ad9a613960e
Shadrack <shadrackbakasa5@gmail.com>
2020-06-02 08:25:05 +0300

    initial app and database coniguration

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -15,14 +15,13 @@
       $url = $this->getUrl();
 
       // Look in controllers for first value
-      if(isset($url[0])){
-        if(file_exists('../app/controllers/' . ucwords($url[0]). '.php')){
-          // If exists, set as controller
-          $this->currentController = ucwords($url[0]);
-          // Unset 0 Index
-          unset($url[0]);
-        }
+      if(file_exists('../app/controllers/' . ucwords($url[0]). '.php')){
+        // If exists, set as controller
+        $this->currentController = ucwords($url[0]);
+        // Unset 0 Index
+        unset($url[0]);
       }
+
       // Require the controller
       require_once '../app/controllers/'. $this->currentController . '.php';
 
